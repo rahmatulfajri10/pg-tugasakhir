04ca803071be752f8594abc3aa76bba56ebe2d52
Rahmatul Fajri <rahmatulfajri445@gmail.com>
2022-12-17 05:51:21 +0700

    cors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors')
 
 const app = express();
 
@@ -21,6 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     res.status(200).json({
